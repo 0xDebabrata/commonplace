f0c970b368fe380f6db4216b4ad216098fd78e3b
Debabrata Mondal <debabrata.js@protonmail.com>
2021-06-20 00:11:12 +0530

    add character limits to textarea and display the limit

--- pages/new.js
+++ pages/new.js
@@ -26,15 +26,25 @@ export default function New() {
                         className={styles.inputExcerpt}
                         type="textarea"
                         placeholder="Add excerpt" 
+                        maxLength="500"
                         value={excerpt}
                         onChange={handleExcerptChange} />
+                    <p className={styles.charLimit}>
+                        {`${500-excerpt.length}/500`}
+                    </p>
+
                     <p className={styles.heading}>Note</p>
                     <textarea
                         className={styles.inputNote}
                         type="textarea"
+                        maxLength="800"
                         placeholder="Add note" 
                         value={note}
                         onChange={handleNoteChange} />
+                    <p className={styles.charLimit}>
+                        {`${800-note.length}/800`}
+                    </p>
+
                     <p className={styles.heading}>Tags</p>
                 </div>
             </div>
