1c74e33533051e5935f3302bb66309854a3b00b8
Debabrata Mondal <debabrata.js@protonmail.com>
2021-06-21 11:16:13 +0530

    add protected route for index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import NewCardButton from '../components/NewCardButton'
+import ProtectedRoute from '../components/ProtectedRoute'
 
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
     return (
-        <div className={styles.container}>
-            <h1>Hello world</h1>
-            <NewCardButton />
-        </div>
+        <ProtectedRoute>
+            <div className={styles.container}>
+                <h1>Hello world</h1>
+                <NewCardButton />
+            </div>
+        </ProtectedRoute>
   )
 }
