566ef18aaaa6889299360554d0196f18a78574ae
Debabrata Mondal <debabrata.js@protonmail.com>
2021-07-07 21:47:24 +0530

    update card styling with glass design

--- components/Tag.js
+++ components/Tag.js
@@ -16,14 +16,17 @@ const Tag = ({ name, colour, preview, setTags, index }) => {
             style={{
                 cursor: 'pointer',
                 height: '30px',
-                paddingLeft: '15px',
-                paddingRight: '15px',
+                paddingLeft: '20px',
+                paddingRight: '20px',
                 background: '#'+colour,
                 color: 'white',
                 display: 'flex',
                 alignItems: 'center',
-                justifyContent: 'center',
+                justifyContent: '10px',
                 borderRadius: '7px',
+                background: 'linear-gradient(180deg, #'+colour+'b0 0%, #'+colour+' 100%)',
+                boxShadow: 'inset 0 0 10px #'+colour, 
+                backdropFilter: 'blur(30px)',
                 margin: '0 5px',
                 marginTop: '5px'
             }}>
