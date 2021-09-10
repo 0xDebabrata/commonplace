a2cbd8795894d48ecfaf12624d487ab8d6d5ce87
Debabrata Mondal <debabrata.js@protonmail.com>
2021-09-10 17:59:09 +0530

    handle no collection exists

--- pages/collection/[id].js
+++ pages/collection/[id].js
@@ -25,6 +25,9 @@ const TagPage = () => {
     // Current collection author 
     const [collectionAuthor, setCollectionAuthor] = useState(null)
 
+    // Whether any card is available
+    const [noCard, setNoCard] = useState(false)
+
     // Get all card details
     const getCard = async (id) => {
         const { data:cards, error } = await supabase
@@ -56,6 +59,29 @@ const TagPage = () => {
             console.log("Error getting card")
             return null
         } 
+
+        // Handle no cards featuring the collection 
+        if (cards.length === 0) {
+
+            // Get collection details
+            const {data: collection, err} = await supabase
+                .from("collections")
+                .select("*")
+                .eq("id", id)
+
+            // If no collection with given id exists
+            if (collection.length === 0) {
+                setNoCard(true)
+                setLoading(false)
+                return
+            }
+
+            setCollectionName(collection[0].name)
+            setCollectionAuthor(collection[0].author)
+            setNoCard(true)
+            setLoading(false)
+            return
+        }
 
         // Produce an array of tags for each card
         cards.forEach(card => {
@@ -92,30 +118,54 @@ const TagPage = () => {
 
                 {!loading && (
                     <>
-                        <div className={styles.wrapper}>
-                            <h2 className={styles.header}>
-                                Collection 
-                            </h2>
-                            <span>
-                                <h2 className={styles.name}>
-                                    {collectionName}
+                        {!noCard && (
+                            <>
+                            <div className={styles.wrapper}>
+                                <h2 className={styles.header}>
+                                    Collection 
                                 </h2>
-                                <h3 className={styles.author}>
-                                    {collectionAuthor}
-                                </h3>
-                            </span>
-                        </div>
-                        {cardArray.map(card => {
-                            return ( 
-                                <Card
-                                    key={card.id}
-                                    excerpt={card.excerpt}
-                                    note={card.note}
-                                    tags={card.card_tag}
-                                    collection={card.collections}
-                                    date={card.created_at} />
-                            )
-                        })}
+                                <span>
+                                    <h2 className={styles.name}>
+                                        {collectionName}
+                                    </h2>
+                                    <h3 className={styles.author}>
+                                        {collectionAuthor}
+                                    </h3>
+                                </span>
+                            </div>
+                            {cardArray.map(card => {
+                                return ( 
+                                    <Card
+                                        key={card.id}
+                                        excerpt={card.excerpt}
+                                        note={card.note}
+                                        tags={card.card_tag}
+                                        collection={card.collections}
+                                        date={card.created_at} />
+                                )
+                            })}
+                            </>
+                        )}
+                        {noCard && collectionName && (
+                            <div className={styles.wrapper}>
+                                <h2 className={styles.header}>
+                                    Collection 
+                                </h2>
+                                <span>
+                                    <h2 className={styles.name}>
+                                        {collectionName}
+                                    </h2>
+                                    <h3 className={styles.author}>
+                                        {collectionAuthor}
+                                    </h3>
+                                </span>
+                            </div>
+                        )}
+                        {noCard && !collectionName && (
+                            <p>
+                                No such collection exists
+                            </p>
+                        )}
                     </>
                 )}
             </div>
