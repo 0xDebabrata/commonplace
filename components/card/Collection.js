2b3a5dcbc9883d835309ce7aacb3e0e940de36f7
Debabrata Mondal <debabrata.js@protonmail.com>
2021-09-10 11:53:53 +0530

    make collection container on card clickable

--- components/card/Collection.js
+++ components/card/Collection.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 import styles from '../../styles/card.module.css'
 
 const Collection = ({ collection }) => {
 
+    const router = useRouter()
+
+    // Redirect to collection page
+    const handleClick = id => {
+        router.push(`/collection/${id}`)
+    }
+
     return (
-        <div className={styles.collectionContainer}>
+        <div 
+            onClick={() => handleClick(collection.id)}
+            className={styles.collectionContainer}>
             <p className={styles.name}>{collection.name}</p>
             <p className={styles.author}>{collection.author}</p>
         </div>
