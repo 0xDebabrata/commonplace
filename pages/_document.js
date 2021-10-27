44ce900a65e20ac6d01e791b339c18aa07c021cd
Debabrata Mondal <debabrata.js@protonmail.com>
2021-10-27 08:42:58 +0530

    add splitbee analytics

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Lora:wght@700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+            <script async src="https://cdn.splitbee.io/sb.js"></script>
         </Head>
         <body>
           <Main />
