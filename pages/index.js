2c8adc495763b4417c63338fe8adddbe88a76046
Debabrata Mondal <debabrata.js@protonmail.com>
2021-12-14 09:36:54 +0530

    add env variables to prod

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useContext } from 'react'
 import { useRouter } from 'next/router'
 import supabase from '../utils/supabaseClient'
+
 import { UserContext } from '../utils/context' 
 import { useKeyPress } from '../utils/hooks'
 import { onKeyPress } from '../functions/keyboard'
