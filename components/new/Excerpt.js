df51d9f7314c9634445da68d3bf92334c615d062
Debabrata Mondal <debabrata.js@protonmail.com>
2021-12-26 12:08:29 +0530

    [md] open links in new tab

--- components/new/Excerpt.js
+++ components/new/Excerpt.js
@@ -8,6 +8,18 @@ const Excerpt = ({ excerpt }) => {
 
     const parseMd = () => {
         const raw = marked(excerpt)
+
+        // Hook to open links in new tab
+        DOMPurify.addHook("afterSanitizeAttributes", (node) => {
+            if ("target" in node) {
+                node.setAttribute("target", "_blank")
+                node.setAttribute("rel", "noopener")
+            }
+            if (!node.hasAttribute('target') && (node.hasAttribute('xlink:href') || node.hasAttribute('href'))) {
+                 node.setAttribute('xlink:show', 'new');
+            }
+        })
+
         const clean = DOMPurify.sanitize(raw)
         return clean
     }
--- components/new/Note.js
+++ components/new/Note.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { marked } from 'marked'
+import DOMPurify from 'dompurify'
 
 import styles from '../../styles/note.module.css'
 
@@ -7,7 +8,20 @@ const Note = ({ note }) => {
 
     const parseMd = () => {
         const raw = marked(note)
-        return raw
+
+        // Hook to open links in new tab
+        DOMPurify.addHook("afterSanitizeAttributes", (node) => {
+            if ("target" in node) {
+                node.setAttribute("target", "_blank")
+                node.setAttribute("rel", "noopener")
+            }
+            if (!node.hasAttribute('target') && (node.hasAttribute('xlink:href') || node.hasAttribute('href'))) {
+                 node.setAttribute('xlink:show', 'new');
+            }
+        })
+
+        const clean = DOMPurify.sanitize(raw)
+        return clean 
     }
 
     return (
