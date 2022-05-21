135dc22087ee300d62aa614e94959c5d9593a663
Debabrata Mondal <debabrata.js@protonmail.com>
2022-05-21 12:08:58 +0530

    add plain text excerpt and note to DB

--- functions/update.js
+++ functions/update.js
@@ -1,224 +1,234 @@
-import supabase from '../utils/supabaseClient'
+import removeMd from "remove-markdown"
+import supabase from "../utils/supabaseClient";
 
 const uncategorised = async (userTags, userId) => {
-    return new Promise(async (resolve, reject) => {
-        let flag = 1 
-        userTags.map(tag => {
-            if (tag.name.toLowerCase() === "uncategorised" || tag.name.toLowerCase() === "uncategorized") {
-                flag = 0
-                resolve(tag.id)
-            } 
-        })
+  return new Promise(async (resolve, reject) => {
+    let flag = 1;
+    userTags.map((tag) => {
+      if (
+        tag.name.toLowerCase() === "uncategorised" ||
+        tag.name.toLowerCase() === "uncategorized"
+      ) {
+        flag = 0;
+        resolve(tag.id);
+      }
+    });
 
-        if (flag) {
-            const { data, error } = await supabase
-                .from("tags")
-                .insert([{
-                    name: "Uncategorised",
-                    colour: "696969",
-                    user_id: userId
-                }])
+    if (flag) {
+      const { data, error } = await supabase.from("tags").insert([
+        {
+          name: "Uncategorised",
+          colour: "696969",
+          user_id: userId,
+        },
+      ]);
 
-            if (error) {
-                throw new Error(error.message)
-            } else {
-                resolve(data[0].id)
-            }
-        }
-    })
-}
+      if (error) {
+        throw new Error(error.message);
+      } else {
+        resolve(data[0].id);
+      }
+    }
+  });
+};
 
-// Insert new tags to tags table and card tags to card_tag table 
+// Insert new tags to tags table and card tags to card_tag table
 // Return array of tag ids for the card
 const handleTags = async (tags, userTags, user_id) => {
-    return new Promise(async (resolve, reject) => {
-        const card_tags = []
-        const newTags = []
-        const tagIds = []
+  return new Promise(async (resolve, reject) => {
+    const card_tags = [];
+    const newTags = [];
+    const tagIds = [];
 
-        if (tags.length === 0) {
-            uncategorised(userTags, user_id)
-                .then(tag_id => {
-                    console.log(tag_id)
-                    console.log({
-                        tagIds: [tag_id], 
-                        card_tags: [{ tag_id, user_id }] 
-                    })
-                    resolve({ 
-                        tagIds: [tag_id], 
-                        card_tags: [{ tag_id, user_id }] 
-                    })
-                })
+    if (tags.length === 0) {
+      uncategorised(userTags, user_id).then((tag_id) => {
+        console.log(tag_id);
+        console.log({
+          tagIds: [tag_id],
+          card_tags: [{ tag_id, user_id }],
+        });
+        resolve({
+          tagIds: [tag_id],
+          card_tags: [{ tag_id, user_id }],
+        });
+      });
+    } else {
+      // Filter newly created tags from user's tags which need to be added to junction table
+      tags.map((tag) => {
+        if (tag.user_id) {
+          // Add tag to newTags for inserting multiple rows
+          newTags.push(tag);
         } else {
-            // Filter newly created tags from user's tags which need to be added to junction table
-            tags.map(tag => {
-                if (tag.user_id) {
-                    // Add tag to newTags for inserting multiple rows
-                    newTags.push(tag)
-                } else {
-                    card_tags.push({
-                        tag_id: tag.id,
-                        user_id
-                    })
+          card_tags.push({
+            tag_id: tag.id,
+            user_id,
+          });
 
-                    tagIds.push(tag.id)
-                }
-            })
-
-            // Insert new tags to tags table
-            const { data, error } = await supabase
-                .from("tags")
-                .insert(newTags)
-
-            if (error) {
-                throw error 
-            }
-
-            // Extract new tag ids to push to newTags
-            data.map(tag => {
-                card_tags.push({
-                    tag_id: tag.id,
-                    user_id
-                })
-
-                tagIds.push(tag.id)
-            })
-
-            resolve({ tagIds, card_tags })
+          tagIds.push(tag.id);
         }
-    })
-}
+      });
 
+      // Insert new tags to tags table
+      const { data, error } = await supabase.from("tags").insert(newTags);
+
+      if (error) {
+        throw error;
+      }
+
+      // Extract new tag ids to push to newTags
+      data.map((tag) => {
+        card_tags.push({
+          tag_id: tag.id,
+          user_id,
+        });
+
+        tagIds.push(tag.id);
+      });
+
+      resolve({ tagIds, card_tags });
+    }
+  });
+};
 
 // Insert rows into card_tag table
 const postToJunctionTable = async (card_tags, card_id) => {
+  card_tags.forEach((tag) => {
+    tag.card_id = card_id;
+  });
 
-    card_tags.forEach(tag => {
-        tag.card_id = card_id
-    })
+  // Insert rows to card_tag junction table
+  const { card_tagData, card_tagError } = await supabase
+    .from("card_tag")
+    .insert(card_tags, { upsert: true });
 
-    // Insert rows to card_tag junction table
-    const { card_tagData, card_tagError } = await supabase
-        .from("card_tag")
-        .insert(card_tags, { upsert: true })
-
-    if (card_tagError) {
-        throw new Error(error)
-    }
-
-}
-
+  if (card_tagError) {
+    throw new Error(error);
+  }
+};
 
 // Return card ID from cards table
 const getCollectionId = async (collection, userCollections, user_id) => {
+  if (!collection.name) return null;
 
-    if (!collection.name) return null
-    
-    let collectionId
-    // Check if collection already exists
-    userCollections.map(userCollection => {
-        if (userCollection.name.toLowerCase() === collection.name.toLowerCase()
-            && userCollection.author.toLowerCase() === collection.author.toLowerCase()) {
-            collectionId = userCollection.id
-        }
-    })
+  let collectionId;
+  // Check if collection already exists
+  userCollections.map((userCollection) => {
+    if (
+      userCollection.name.toLowerCase() === collection.name.toLowerCase() &&
+      userCollection.author.toLowerCase() === collection.author.toLowerCase()
+    ) {
+      collectionId = userCollection.id;
+    }
+  });
 
-    // If collection does not exist, insert it to collections table
-    if (!collectionId) {
-        collection.user_id = user_id
-        const { data, error } = await supabase
-            .from("collections")
-            .insert([collection])
+  // If collection does not exist, insert it to collections table
+  if (!collectionId) {
+    collection.user_id = user_id;
+    const { data, error } = await supabase
+      .from("collections")
+      .insert([collection]);
 
-        if (error) {
-            throw error
-        }
-
-        collectionId = data[0].id
+    if (error) {
+      throw error;
     }
 
-    return collectionId
+    collectionId = data[0].id;
+  }
 
-}
+  return collectionId;
+};
 
 // Delete removed tags from card_tag junction table
 const deleteJunctionRows = async (card_id, tag_id) => {
-    const { data, error } = await supabase
-        .from("card_tag")
-        .delete()
-        .match({ card_id, tag_id })
+  const { data, error } = await supabase
+    .from("card_tag")
+    .delete()
+    .match({ card_id, tag_id });
 
-    if (error) {
-        throw error
-    }
-}
+  if (error) {
+    throw error;
+  }
+};
 
 // Check if user is a valid customer
 const isCustomer = async (creationDate) => {
-    return new Promise(async (resolve, reject) => {
-        const start = new Date(creationDate)
-        const today = new Date()
+  return new Promise(async (resolve, reject) => {
+    const start = new Date(creationDate);
+    const today = new Date();
 
-        const days = (today-start)/86400000
+    const days = (today - start) / 86400000;
 
-        if (days >= 8) {
-            const { data } = await supabase
-                .from("users")
-                .select("customer_id")
+    if (days >= 8) {
+      const { data } = await supabase.from("users").select("customer_id");
 
-            if (data[0].customer_id) {
-                resolve(true)
-            } else {
-                resolve(false)
-            }
-        } else {
-            // Free trial still valid so return true
-            resolve(true)
-        }
-    })
-}
+      if (data[0].customer_id) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    } else {
+      // Free trial still valid so return true
+      resolve(true);
+    }
+  });
+};
 
 // Update card
-export const updateCard = async (excerpt, note, collection, userCollections, tags, userTags, date, cardId, deleteRows) => {
+export const updateCard = async (
+  excerpt,
+  note,
+  collection,
+  userCollections,
+  tags,
+  userTags,
+  date,
+  cardId,
+  deleteRows
+) => {
+  // Check if user is a customer
+  const customer = await isCustomer(supabase.auth.user().created_at);
+  if (!customer) {
+    throw new Error("Your free trial has ended 🙁");
+  }
 
-    // Check if user is a customer
-    const customer = await isCustomer(supabase.auth.user().created_at)
-    if (!customer) {
-        throw new Error("Your free trial has ended 🙁")
-    }
+  // Delete rows from junction table
+  deleteRows.forEach(async (id) => {
+    await deleteJunctionRows(cardId, id);
+  });
 
-    // Delete rows from junction table
-    deleteRows.forEach(async (id) => {
-        await deleteJunctionRows(cardId, id)
-    })
+  const card = {};
+  const user_id = supabase.auth.user().id;
 
-    const card = {}
-    const user_id = supabase.auth.user().id
+  if (!note && !excerpt) {
+    throw new Error("Please add a note or an excerpt");
+  }
 
-    if (!note && !excerpt) {
-        throw new Error("Please add a note or an excerpt")
-    }
+  const { tagIds, card_tags } = await handleTags(tags, userTags, user_id);
 
-    const { tagIds, card_tags } = await handleTags(tags, userTags, user_id)
+  card.user_id = user_id;
+  card.collection_id = await getCollectionId(
+    collection,
+    userCollections,
+    user_id
+  );
+  card.excerpt = excerpt;
+  card.note = note;
+  card.tags = tagIds;
+  card.plain = {
+    excerpt: removeMd(excerpt),
+    note: removeMd(note)
+  }
 
-    card.user_id = user_id
-    card.collection_id = await getCollectionId(collection, userCollections, user_id)
-    card.excerpt = excerpt
-    card.note = note
-    card.tags = tagIds 
+  card.created_at = date;
 
-    card.created_at = date
+  const { error } = await supabase
+    .from("cards")
+    .update(card, { returning: "minimal" })
+    .eq("id", cardId);
 
-    const { data, error } = await supabase
-        .from("cards")
-        .update(card)
-        .eq("id", cardId)
+  if (error) {
+    throw error;
+  }
 
-    if (error) {
-        throw error
-    } 
-
-    await postToJunctionTable(card_tags, cardId)
-}
-
-
+  await postToJunctionTable(card_tags, cardId);
+};
--- pages/new.js
+++ pages/new.js
@@ -1,141 +1,152 @@
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import ProtectedRoute from '../components/ProtectedRoute'
-import supabase from '../utils/supabaseClient'
-import toast from 'react-hot-toast'
-import { useKeyPress } from '../utils/hooks'
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import ProtectedRoute from "../components/ProtectedRoute";
+import supabase from "../utils/supabaseClient";
+import toast from "react-hot-toast";
+import { useKeyPress } from "../utils/hooks";
 
-import { createCard } from '../functions/new'
+import { createCard } from "../functions/new";
 
-import EditingView from '../components/new/EditingView'
-import PreviewCard from '../components/new/PreviewCard'
-import styles from '../styles/new.module.css'
+import EditingView from "../components/new/EditingView";
+import PreviewCard from "../components/new/PreviewCard";
+import styles from "../styles/new.module.css";
 
 export default function New() {
+  const router = useRouter();
 
-    const router = useRouter()
+  const [excerpt, setExcerpt] = useState("");
+  const [note, setNote] = useState("");
 
-    const [excerpt, setExcerpt] = useState('')
-    const [note, setNote] = useState('')
+  // Loading user's tags and collections from DB
+  const [tagsLoading, setTagsLoading] = useState(true);
+  const [collectionsLoading, setCollectionsLoading] = useState(true);
 
-    // Loading user's tags and collections from DB
-    const [tagsLoading, setTagsLoading] = useState(true)
-    const [collectionsLoading, setCollectionsLoading] = useState(true)
+  // User's tags and collections
+  const [userTags, setUserTags] = useState(null);
+  const [userCollections, setUserCollections] = useState(null);
 
-    // User's tags and collections
-    const [userTags, setUserTags] = useState(null)
-    const [userCollections, setUserCollections] = useState(null)
+  // Current tags and collection
+  const [tags, setTags] = useState([]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
 
-    // Current tags and collection
-    const [tags, setTags] = useState([])
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
+  // Set keyboard shortcuts
+  useKeyPress(["S"], () => handleKeyPress());
 
-    // Set keyboard shortcuts
-    useKeyPress(['S'], () => handleKeyPress())
+  const handleKeyPress = () => {
+    const textarea = document.getElementsByTagName("textarea");
+    const inputs = document.getElementsByTagName("input");
+    const textareaArr = Array.from(textarea);
+    const inputArr = Array.from(inputs);
 
-    const handleKeyPress = () => {
-        const textarea = document.getElementsByTagName("textarea")
-        const inputs = document.getElementsByTagName("input")
-        const textareaArr = Array.from(textarea)
-        const inputArr = Array.from(inputs)
-
-        if (!inputArr.includes(document.activeElement) && !textareaArr.includes(document.activeElement)) {
-            handleCreateCard()
-        }
+    if (
+      !inputArr.includes(document.activeElement) &&
+      !textareaArr.includes(document.activeElement)
+    ) {
+      handleCreateCard();
     }
+  };
 
-    const handleExcerptChange = () => {
-        const excerptVal = document.getElementsByTagName("textarea")[0].value
-        setExcerpt(excerptVal)
-    }
+  const handleExcerptChange = () => {
+    const excerptVal = document.getElementsByTagName("textarea")[0].value;
+    setExcerpt(excerptVal);
+  };
 
-    const handleNoteChange = () => {
-        const noteVal = document.getElementsByTagName("textarea")[1].value
-        setNote(noteVal)
-    }
+  const handleNoteChange = () => {
+    const noteVal = document.getElementsByTagName("textarea")[1].value;
+    setNote(noteVal);
+  };
 
-    // Get user tags
-    const getTags = async () => {
-        let { data: tags, error } = await supabase
-            .from('tags')
-            .select('*')
-        
-        setUserTags(tags)
-        setTagsLoading(false)
-    }
+  // Get user tags
+  const getTags = async () => {
+    let { data: tags, error } = await supabase.from("tags").select("*");
 
-    const getCollections = async () => {
-        let { data: collections, error } = await supabase
-            .from('collections')
-            .select('*')
-        
-        setUserCollections(collections)
-        setCollectionsLoading(false)
-    }
+    setUserTags(tags);
+    setTagsLoading(false);
+  };
 
-    useEffect(() => {
-        getCollections()
-        getTags()
-    }, [])
+  const getCollections = async () => {
+    let { data: collections, error } = await supabase
+      .from("collections")
+      .select("*");
 
-    const handleCreateCard = () => {
-        const collection = {name: title, author}
-        const promise = createCard(excerpt, note, collection, userCollections, tags, userTags)
-        toast.promise(promise, {
-            loading: "Creating card",
-            success: data => {
-                router.push(`/card/${data}`)
-                return "Card created" 
-            },
-            error: err => {
-                return `${err}`
-            } 
+    setUserCollections(collections);
+    setCollectionsLoading(false);
+  };
+
+  useEffect(() => {
+    getCollections();
+    getTags();
+  }, []);
+
+  const handleCreateCard = () => {
+    const collection = { name: title, author };
+    const promise = createCard(
+      excerpt,
+      note,
+      collection,
+      userCollections,
+      tags,
+      userTags
+    );
+    toast.promise(
+      promise,
+      {
+        loading: "Creating card",
+        success: (data) => {
+          router.push(`/card/${data}`);
+          return "Card created";
         },
-        {
-            style: {
-                background: "rgba(105,105,105,0.7)",
-                minWidth: "300px",
-                color: "white",
-                backdropFilter: "blur(10px)"
-            }
-        })
-    }
+        error: (err) => {
+          console.error(err)
+          return `${err.message}`;
+        },
+      },
+      {
+        style: {
+          background: "rgba(105,105,105,0.7)",
+          minWidth: "300px",
+          color: "white",
+          backdropFilter: "blur(10px)",
+        },
+      }
+    );
+  };
 
-    return (
-        <ProtectedRoute>
-            <div className={styles.container}>
-                <EditingView 
-                    newCard={true}
-                    userTags={userTags}
-                    tags={tags}
-                    setTags={setTags}
-                    tagsLoading={tagsLoading}
+  return (
+    <ProtectedRoute>
+      <div className={styles.container}>
+        <EditingView
+          newCard={true}
+          userTags={userTags}
+          tags={tags}
+          setTags={setTags}
+          tagsLoading={tagsLoading}
+          userCollections={userCollections}
+          collectionsLoading={collectionsLoading}
+          title={title}
+          setTitle={setTitle}
+          author={author}
+          setAuthor={setAuthor}
+          note={note}
+          excerpt={excerpt}
+          handleNoteChange={handleNoteChange}
+          handleExcerptChange={handleExcerptChange}
+        />
 
-                    userCollections={userCollections}
-                    collectionsLoading={collectionsLoading}
-                    title={title}
-                    setTitle={setTitle}
-                    author={author}
-                    setAuthor={setAuthor}
-
-                    note={note}
-                    excerpt={excerpt}
-                    handleNoteChange={handleNoteChange}
-                    handleExcerptChange={handleExcerptChange} />
-
-                <div className={styles.previewContainer}>
-                    <div className={styles.previewBox}>
-                        <h2 className={styles.header}>Preview</h2>
-                        <PreviewCard excerpt={excerpt} note={note} />
-                        <button
-                            className={styles.button}
-                            onClick={() => handleCreateCard()}>
-                            Create card
-                        </button>
-                    </div>
-                </div> 
-            </div>
-        </ProtectedRoute>
-    )
- }
+        <div className={styles.previewContainer}>
+          <div className={styles.previewBox}>
+            <h2 className={styles.header}>Preview</h2>
+            <PreviewCard excerpt={excerpt} note={note} />
+            <button
+              className={styles.button}
+              onClick={() => handleCreateCard()}
+            >
+              Create card
+            </button>
+          </div>
+        </div>
+      </div>
+    </ProtectedRoute>
+  );
+}
