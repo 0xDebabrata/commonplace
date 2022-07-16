49fecfa821bbde16ce7862f41d5218b715601591
Debabrata Mondal <debabrata.js@protonmail.com>
2022-07-16 20:49:10 +0530

    add supabase next-auth and update index

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,30 @@
-import { UserContext } from '../utils/context'
-import { useUserData } from '../utils/hooks'
+import { UserProvider } from "@supabase/auth-helpers-react"
+import { supabaseClient } from "@supabase/auth-helpers-nextjs"
+import { UserContext } from "../utils/context";
+import { useUserData } from "../utils/hooks";
 
-import { Toaster } from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 
-import Banner from "../components/Banner"
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Banner from "../components/Banner";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
 
-    const userData = useUserData()
-
-    return (
-        <UserContext.Provider value={ userData }>
-            <Banner />
-            <Navbar />
-            <Component {...pageProps} />
-            <Footer />
-            <Toaster />
-        </UserContext.Provider>
-    )
+  return (
+    <UserProvider supabaseClient={supabaseClient}>
+      <UserContext.Provider value={userData}>
+        <Banner />
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+        <Toaster />
+      </UserContext.Provider>
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
