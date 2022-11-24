15ad15ebcce4010cc87a1a9071ffe603a23cb448
Debabrata Mondal <debabrata.js@protonmail.com>
2022-11-24 19:43:30 +0530

    allow data exports

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,22 @@
 import Shortcuts from "./keyboard-shortcuts/Shortcuts"
+import Export from "./export/"
 
 import styles from '../styles/footer.module.css'
 
 const Footer = () => {
-    return (
-        <div className={styles.container}>
-            <Shortcuts />
-            <ul>
-                <a href="mailto:dev@commonplace.one"><li>Report a bug</li></a>
-                <a href="mailto:hello@commonplace.one"><li>Contact</li></a>
-            </ul>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Shortcuts />
+        <Export />
+      </div>
+
+      <ul>
+        <a href="mailto:dev@commonplace.one"><li>Report a bug</li></a>
+        <a href="mailto:hello@commonplace.one"><li>Contact</li></a>
+      </ul>
+    </div>
+  )
 }
 
 export default Footer
---
+++ components/export/index.js
@@ -0,0 +1,96 @@
+import { useUser } from "@supabase/auth-helpers-react"
+import { supabaseClient } from "@supabase/auth-helpers-nextjs"
+
+import styles from '../../styles/shortcuts.module.css'
+
+function convertToCSV(objArray) {
+  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  var str = array[0].join(",") + '\r\n';
+
+  for (var i = 1; i < array.length; i++) {
+    var line = '';
+    for (var index in array[i]) {
+      if (line != '') line += ','
+
+      let cell = array[i][index]
+      line += `\"${cell?.toString().replace(/\n/g, "\\n")}\"`;
+    }
+
+    str += line + '\r\n';
+  }
+
+  return str;
+}
+
+function exportCSVFile(headers, items, filename) {
+  items.unshift(headers);
+  // Convert Object to JSON
+  var jsonObject = JSON.stringify(items);
+
+  var csv = convertToCSV(jsonObject);
+
+  var exportedFilenmae = filename + ".csv"
+
+  var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+    navigator.msSaveBlob(blob, exportedFilenmae);
+  } else {
+    var link = document.createElement("a");
+    if (link.download !== undefined) { // feature detection
+      // Browsers that support HTML5 download attribute
+      var url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", exportedFilenmae);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+}
+
+const Export = () => {
+  const { user } = useUser()
+  const exportData = async () => {
+    const { data } = await supabaseClient
+      .from("cards")
+      .select(`id, excerpt, note, created_at, tags, collections (
+        collection
+      ) `)
+
+    const items = data.map(card => {
+      const { id, excerpt, note, created_at, tags, collections } = card
+      return {
+        id, excerpt, note, created_at,
+        tags: tags.join(","),
+        collection: JSON.stringify(collections?.collection)
+      }
+    })
+    const headers = ["id, excerpt, note, created_at, tags, collection"]
+    exportCSVFile(headers, items, "commonplace-cards-export")
+
+    const { data: tags } = await supabaseClient
+      .from("tags")
+      .select(`id, name, colour`)
+
+    const tagHeaders = ["id, name, colour"]
+    exportCSVFile(tagHeaders, tags, "commonplace-tags-export")
+  }
+
+  if (user) {
+    return (
+      <>
+        <p
+          className={styles.footerText}
+          onClick={exportData}
+        >
+          Export
+        </p>
+      </>
+    )
+  } else {
+    return null;
+  }
+}
+
+export default Export
