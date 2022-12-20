6da85bddfad2a16abb1f6ed9233a220b06993633
Debabrata Mondal <debabrata.js@protonmail.com>
2022-12-20 16:54:57 +0530

    handle single card views and card deletion

--- components/card/Delete.js
+++ components/card/Delete.js
@@ -1,5 +1,6 @@
 import React, { useState, forwardRef, useImperativeHandle } from 'react'
 import { useRouter } from 'next/router'
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
 import toast from 'react-hot-toast'
 
 import DeleteModal from '../DeleteModal'
@@ -7,62 +8,62 @@ import DeleteModal from '../DeleteModal'
 import styles from '../../styles/card.module.css'
 
 const DeleteCard = forwardRef(({ id, deleteFunc }, parentRef) => {
+  const router = useRouter()
+  const supabaseClient = useSupabaseClient()
 
-    const router = useRouter()
+  // Modal state
+  const [open, setOpen] = useState(false)
 
-    // Modal state
-    const [open, setOpen] = useState(false)
-
-    const handleDelete = (id, router) => {
-        const promise = deleteFunc(id, router)
-        toast.promise(promise, {
-            loading: "Deleting card",
-            success: "Card deleted", 
-            error: err => {
-                return `${err}`
-            } 
+  const handleDelete = (id, router) => {
+    const promise = deleteFunc(supabaseClient, id, router)
+    toast.promise(promise, {
+      loading: "Deleting card",
+      success: "Card deleted", 
+      error: err => {
+        return `${err}`
+      } 
+    },
+      {
+        style: {
+          background: "rgba(105,105,105,0.7)",
+          minWidth: "300px",
+          color: "white",
+          backdropFilter: "blur(10px)"
         },
-        {
-            style: {
-                background: "rgba(105,105,105,0.7)",
-                minWidth: "300px",
-                color: "white",
-                backdropFilter: "blur(10px)"
-            },
-            success: {
-                icon: "🗑"
-            }
-        })
-    }
-
-    useImperativeHandle(parentRef, () => ({
-        openDeleteModal() {
-            setOpen(true)
+        success: {
+          icon: "🗑"
         }
-    }))
+      })
+  }
 
-    return(
-        <div className={styles.deleteContainer} >
-            <img 
-                onClick={() => router.push(`/edit?id=${id}`)}
-                src="/edit-icon.svg" 
-                alt="edit card icon" 
-            />
-            <img 
-                onClick={() => setOpen(true)}
-                src="/delete-icon.svg" 
-                alt="delete card icon" />
+  useImperativeHandle(parentRef, () => ({
+    openDeleteModal() {
+      setOpen(true)
+    }
+  }))
 
-            <DeleteModal
-                type="card"
-                open={open}
-                setOpen={setOpen}
-                handleDelete={handleDelete}
-                id={id}
-                router={router}
-            />
-        </div>
-    )
+  return(
+    <div className={styles.deleteContainer} >
+      <img 
+        onClick={() => router.push(`/edit?id=${id}`)}
+        src="/edit-icon.svg" 
+        alt="edit card icon" 
+        />
+      <img 
+        onClick={() => setOpen(true)}
+        src="/delete-icon.svg" 
+        alt="delete card icon" />
+
+      <DeleteModal
+        type="card"
+        open={open}
+        setOpen={setOpen}
+        handleDelete={handleDelete}
+        id={id}
+        router={router}
+        />
+    </div>
+  )
 })
 
 export default DeleteCard
