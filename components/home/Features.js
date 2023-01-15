8c0f7a19cd499ff00647715d1354b1e9ddb4984c
Debabrata Mondal <debabrata.js@protonmail.com>
2023-01-15 15:34:47 +0530

    Update website copy

--- components/home/Features.js
+++ components/home/Features.js
@@ -10,7 +10,7 @@ export default function Features() {
         Search
       </h2>
       <h3 className="text-2xl mt-2 text-center text-slate-400">
-        Search for an idea. Unearth relevant information.
+        Google, but for your information
       </h3>
         <Image src="/search.png" alt="Commonplace search" width={878} height={500}
           className="mx-auto mt-8 hidden md:block px-10"
@@ -18,12 +18,11 @@ export default function Features() {
       <div className="pt-10 px-10 max-w-5xl mx-auto text-center">
         <div className="flex flex-col justify-between px-10">
           <p className="text-white text-2xl mt-4">
-            You probably have multiple bookmarks with no way of finding what you need.
+            Save information from multiple sources and always find what you need.
           </p>
           <p className="text-slate-300 text-lg mt-4 font-thin">
-            Chances are you have forgotten what you had bookmarked on Twitter. Re-discover and find the stuff you need with a powerful search that is a master at providing relevant information.
-            <br /><br />
-            "Learn programming"<br /> "Healthy diet for teens"<br /> Google, but for your information
+            We consume content from various sources but there is no way of finding that article you came across last week.
+            Simply save it to Commonplace and we&apos;ll make sure you find it when you need it.
           </p>
         </div>
       </div>
@@ -32,7 +31,7 @@ export default function Features() {
         Smart Collections
       </h2>
       <h3 className="text-2xl mt-2 text-center text-slate-400">
-        No time to manually organize bookmarks? We got you.
+        No time to manually organize your information? We got you.
       </h3>
       <div className="pt-16 px-10 max-w-5xl mx-auto flex flex-col md:flex-row items-center md:justify-between">
         <div className="min-w-[400px] lg:min-w-[500px]">
@@ -43,10 +42,10 @@ export default function Features() {
         </div>
         <div className="flex flex-col justify-between px-10">
           <p className="text-white ml-10 text-2xl mt-4">
-            Commonplace automatically organizes all your information into smart collections so you don't have to.
+            Commonplace automatically organizes everything based on topics
           </p>
           <p className="text-slate-300 md:ml-10 text-lg mt-4">
-            All you need to do is search to find the relevant information. We handle the rest.
+            You don&apos;t need to worry about coming up with the perfect way to organize your information.
           </p>
         </div>
       </div>
@@ -58,11 +57,8 @@ export default function Features() {
         Reflect. Remember. Learn.
       </h3>
       <div className="pt-16 px-10 max-w-5xl text-center mx-auto">
-        <p className="text-white text-2xl mt-4">
-          It's important that you are in touch with everything in your personal library.
-        </p>
         <p className="text-slate-300 text-lg mt-4">
-          Get weekly highlights and summary of all the twitter bookmarks you add straight to your inbox.
+          Get weekly highlights and summaries for your information straight to your inbox.
         </p>
       </div>
     </div>
--- components/home/One.js
+++ components/home/One.js
@@ -21,20 +21,15 @@ const One = () => {
         />
 
       <h1 className="text-5xl mt-3">
-        Your information, organized.
+        Save articles and tweets for easy access
       </h1>
       <h3 className="text-2xl max-w-3xl mt-6 px-5 mx-auto text-slate-300">
-        {/*Capture information and remember what matters to you the most.<br />*/}
-        Home for your Twitter bookmarks.<br />
-        With a powerful search built-in, your information is never lost.
+        Commonplace automatically organizes your information and has a powerful search built-in
       </h3>
 
       <button onClick={signIn} className="py-2 px-10 mt-8 bg-neutral-600 rounded cursor-pointer transition-all hover:bg-neutral-500">
-        Sign up
+        Grow your information library
       </button>
-      <p className="text-sm text-slate-400 mt-2">
-        Public beta
-      </p>
     </div>
   )
 };
