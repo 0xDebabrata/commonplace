388deada6bbdc2d3246f2a4a0d62c6cff4830452
Debabrata Mondal <debabrata.js@protonmail.com>
2023-02-01 20:30:48 +0530

    update new card icon

--- components/NewCardButton.js
+++ components/NewCardButton.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 import { IconContext } from "react-icons"
-import { TfiWorld } from "react-icons/tfi"
+import { AiOutlinePlus } from "react-icons/ai"
 
 const NewCardButton = () => {
   return (
     <Link href='/web'>
-      <div className="fixed bottom-8 right-10 bg-zinc-300 p-2 rounded-full hover:brightness-150 duration-150">
+      <div title="Summarize articles" className="fixed bottom-8 right-10 bg-zinc-300 p-2 rounded-full hover:brightness-150 duration-150">
         <IconContext.Provider value={{ className: "text-stone-600 text-2xl" }}>
-          <TfiWorld title="Summarize articles" />
+          <AiOutlinePlus />
         </IconContext.Provider>
       </div>
     </Link>
